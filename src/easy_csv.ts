dd6ecfd7fa36b8bed8209f9bc5f7fea452d84d58
市川恭佑 <github@mail.ebi-yade.com>
2022-05-03 12:47:21 +0900

    Add an answer of the quiz 3.8

--- src/easy_csv.ts
+++ src/easy_csv.ts
@@ -10,7 +10,16 @@ John Smith,17,0
 Mary Sue,14,1
 `
 
-const users: User[] = []
+const users: User[] = data.split('\n')
+.filter(row => !!row.trim())
+.map(
+    (row: string): User => {
+        const [name, ageStr, boolStr] = row.split(',');
+        const age = Number(ageStr);
+        const premiumUser = boolStr === '1';
+        return {name, age, premiumUser};
+    }
+);
 
 for (const user of users) {
     console.log(`${user.name} (${user.age})はプレミアムユーザー${user.premiumUser ? 'です' : 'ではありません'}。`)
